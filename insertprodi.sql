98aece4492a26bab4706a8a2a2b655b4e445d355
Dayana Sheilla Agathy <Dayana Sheilla Agathy>
2016-11-18 13:43:08 +0700

    commit

---
+++ insertprodi.sql
@@ -0,0 +1,7 @@
+insert into prodi (Id, NamaProdi) values
+('1','Ilmu Komputer'),
+('2','Sistem Informasi'),
+('3','Matematika'),
+('4','Teknik Komputer'),
+('5','Teknik Industri'),
+('6','Manajemen');
---
+++ insertterm.sql
@@ -0,0 +1,4 @@
+insert into Term (Tahun, Semester) values
+('2016','1'),
+('2016','2'),
+('2016','3');
